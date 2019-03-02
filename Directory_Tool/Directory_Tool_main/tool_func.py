34bc4b7db2b019ae35cab32856958c9d94c5edf9
FiveStringJazz <42543340+FiveStringJazz@users.noreply.github.com>
2019-03-01 23:14:33 -0600

    fixed bugs

--- Directory_Tool/Directory_Tool_main/tool_func.py
+++ Directory_Tool/Directory_Tool_main/tool_func.py
@@ -7,4 +7,14 @@ import tool_gui
 
 
 def search_dir(self):
+
+    def search_dir(self):
+    directory = askdirectory()
+    if directory:
+        self.varDir.set(directory)
+
+def search_dest(self):
+    destination = askdirectory()
+    if directory:
+        self.varDest.set(destination)
     
